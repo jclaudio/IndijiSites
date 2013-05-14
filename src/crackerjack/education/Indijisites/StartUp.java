b197f28ac13e5d033964589664a5a7b4e7f719a7
Jonathan Claudio <jonathan.claudio@outlook.com>
2013-05-14 21:52:59 +1000

    Initial Commit

---
+++ src/crackerjack/education/Indijisites/StartUp.java
@@ -0,0 +1,40 @@
+package crackerjack.education.Indijisites;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.app.AlertDialog;
+import android.view.MenuItem;
+
+public class StartUp extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.application_start_up);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start_up, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_legalnotices:
+			String LicenseInfo = GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(getApplicationContext());
+			AlertDialog.Builder LicenseDialog = new AlertDialog.Builder(StartUp.this);
+			LicenseDialog.setTitle("Legal Notices");
+			LicenseDialog.setMessage(LicenseInfo);
+			LicenseDialog.show();
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
